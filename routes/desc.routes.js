2b0cf42c0557d8c896f31b2a40b3d9fc8e9fb6a6
molodec08 <molodec.08@mail.ru>
2022-06-07 20:43:52 +0300

    Add map

--- routes/desc.routes.js
+++ routes/desc.routes.js
@@ -1,10 +1,20 @@
 const {Router} = require("express");
 const Description = require("../models/Description");
+const auth = require('../middleware/auth.middleware');
 const router = Router();
 
 router.get('/last-desc', async (req, res) => {
   try {
     const desc = await Description.findOne().sort('-date');
+    res.json(desc)
+  } catch (e) {
+    res.status(500).json( { message: 'Что-то пошло не так, попробуйте снова' })
+  }
+});
+
+router.get('/all-revision', async (req, res) => {
+  try {
+    const desc = await Description.find().sort('-date');
     res.json(desc)
   } catch (e) {
     res.status(500).json( { message: 'Что-то пошло не так, попробуйте снова' })
